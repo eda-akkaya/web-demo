700637e4f3dbc1f8da3c0977391f4278b85da48b
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-13 23:54:25 +0300

    Controller operations added: @GetMapping, @RequestParam

--- src/main/java/com/example/web_demo/controller/TestController.java
+++ src/main/java/com/example/web_demo/controller/TestController.java
@@ -2,14 +2,16 @@ package com.example.web_demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test") // Tüm isteklere ön ek ekler.
 public class TestController {
 
-    @GetMapping("/hello")
-    public String sayHi(){
-        return "merhaba";
+    @GetMapping({"/hello","/merhaba"})
+    public String sayHi(@RequestParam(defaultValue="kullanıcı") String name,
+                        @RequestParam() int age){ // default query string
+        return "Merhaba" + name + ", yaşınız: " + age;
     }
 }
