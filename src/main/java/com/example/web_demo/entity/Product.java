d3ab26cc25662b832deea99f5c6ae48db5727945
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-14 23:44:26 +0300

    jakarta notations added: Entity, Table

--- src/main/java/com/example/web_demo/entity/Product.java
+++ src/main/java/com/example/web_demo/entity/Product.java
@@ -1,10 +1,25 @@
 package com.example.web_demo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="products")
 public class Product {
+    @Id
+    @Column(name="id") // aynı ismi kullanacaksan gerek yok
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
+
+    @Column(name="unit_price")
     private float price;
+
     private float stock;
+
+    @Column(name="description")
+    private String description;
+
 
     public int getId() {
         return id;
@@ -36,5 +51,13 @@ public class Product {
 
     public void setStock(float stock) {
         this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
