15a98dc6b4cef223a7e9bf0b08d14e05abefaed9
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-15 23:40:28 +0300

    rearranged ProductsController methods according to ProductService methods

--- src/main/java/com/example/web_demo/controller/ProductsController.java
+++ src/main/java/com/example/web_demo/controller/ProductsController.java
@@ -2,6 +2,7 @@ package com.example.web_demo.controller;
 
 import com.example.web_demo.entity.Product;
 import com.example.web_demo.repository.ProductRepository;
+import com.example.web_demo.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,22 +13,22 @@ import java.util.Random;
 @RestController //Spring tarafından RestController olarak tanınsın
 @RequestMapping("/api/v1/products") // localhost:port/api/v1/products ile başlıyorsa istek buraya gelir.
 public class ProductsController {
-    private ProductRepository productRepository;
-
-    public ProductsController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    // dependency injection
+    private ProductService productService;
+    public ProductsController(ProductService productService){
+        this.productService = productService;
     }
 
     @GetMapping()
     public List<Product> getAll(){
-        return productRepository.findAll();
+        return productService.getAll();
     }
 
     //best-practice: Ekleme endpointleri ekleme sonrası entity'i geri döner.
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)// işlem başarılı olursa status code olarak bunu döner
     public Product add(@RequestBody Product product){
-        productRepository.save(product);
-        return product;
+        return productService.add(product);
+
     }
 }
