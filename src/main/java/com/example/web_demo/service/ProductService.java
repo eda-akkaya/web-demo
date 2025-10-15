d108a37dad9d327851cd4ca64dab7a1fc62c9e99
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-15 23:39:24 +0300

    ProductService added

---
+++ src/main/java/com/example/web_demo/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.example.web_demo.service;
+
+import com.example.web_demo.entity.Product;
+
+import java.util.List;
+
+public class ProductService {
+
+    public List<Product> getAll(){
+        return null;
+    }
+
+    public Product add(Product product){
+        return product;
+    }
+
+}
