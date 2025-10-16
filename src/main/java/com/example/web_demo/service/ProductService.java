6141405cae3480fb46ac70412cda0dc28265f671
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-16 23:54:40 +0300

    manuel product DTO mapping added

--- src/main/java/com/example/web_demo/service/ProductService.java
+++ src/main/java/com/example/web_demo/service/ProductService.java
@@ -1,17 +1,35 @@
 package com.example.web_demo.service;
 
+import com.example.web_demo.dto.product.ProductForAddDto;
+import com.example.web_demo.entity.Category;
 import com.example.web_demo.entity.Product;
+import com.example.web_demo.repository.ProductRepository;
 
 import java.util.List;
+public class ProductService{
 
-public class ProductService {
+    private ProductRepository productRepository;
 
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
     public List<Product> getAll(){
         return null;
     }
 
-    public Product add(Product product){
-        return product;
-    }
+    public Product add(ProductForAddDto productForAddDto){
+        //mapping
+        Product product = new Product();
+        product.setName(productForAddDto.getName());
+        product.setDescription(productForAddDto.getDescription());
+        product.setStock(productForAddDto.getStock());
+        product.setPrice(productForAddDto.getUnitPrice());
 
+        Category category = new Category();
+        category.setId(productForAddDto.getCategoryId());
+
+        product.setCategory(category);
+        return this.productRepository.save(product);
+
+    }
 }
