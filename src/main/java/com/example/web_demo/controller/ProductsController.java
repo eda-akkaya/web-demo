111a51d4893650a56c292179d1a44beef7812d40
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-17 23:40:02 +0300

    post endpoint added

--- src/main/java/com/example/web_demo/controller/ProductsController.java
+++ src/main/java/com/example/web_demo/controller/ProductsController.java
@@ -1,9 +1,24 @@
 package com.example.web_demo.controller;
 
+import com.example.web_demo.dto.product.request.CreateProductRequest;
+import com.example.web_demo.dto.product.response.CreatedProductResponse;
+import com.example.web_demo.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController //Spring tarafından RestController olarak tanınsın
 @RequestMapping("/api/v1/products") // localhost:port/api/v1/products ile başlıyorsa istek buraya gelir.
 public class ProductsController {
 
+    private final ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreatedProductResponse add(@RequestBody CreateProductRequest createProductRequest){
+        return productService.add(createProductRequest);
+    }
 }
