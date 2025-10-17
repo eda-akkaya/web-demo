4d88b1044bb96a19a60556622c1eb4d32b5cd187
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-17 23:03:19 +0300

    ProductController and ProductService codes were deleted, request-response pattern will be written

--- src/main/java/com/example/web_demo/controller/ProductsController.java
+++ src/main/java/com/example/web_demo/controller/ProductsController.java
@@ -1,35 +1,9 @@
 package com.example.web_demo.controller;
 
-import com.example.web_demo.dto.product.ProductForAddDto;
-import com.example.web_demo.entity.Product;
-import com.example.web_demo.repository.ProductRepository;
-import com.example.web_demo.service.ProductService;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 @RestController //Spring tarafından RestController olarak tanınsın
 @RequestMapping("/api/v1/products") // localhost:port/api/v1/products ile başlıyorsa istek buraya gelir.
 public class ProductsController {
-    // dependency injection
-    private ProductService productService;
-    public ProductsController(ProductService productService){
-        this.productService = productService;
-    }
 
-    @GetMapping()
-    public List<Product> getAll(){
-        return productService.getAll();
-    }
-
-    //best-practice: Ekleme endpointleri ekleme sonrası entity'i geri döner.
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)// işlem başarılı olursa status code olarak bunu döner
-    public Product add(@RequestBody ProductForAddDto productDto){
-        return productService.add(productDto);
-
-    }
 }
--- src/main/java/com/example/web_demo/service/ProductService.java
+++ src/main/java/com/example/web_demo/service/ProductService.java
@@ -1,35 +1,8 @@
 package com.example.web_demo.service;
 
-import com.example.web_demo.dto.product.ProductForAddDto;
-import com.example.web_demo.entity.Category;
-import com.example.web_demo.entity.Product;
-import com.example.web_demo.repository.ProductRepository;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
+@Service
 public class ProductService{
 
-    private ProductRepository productRepository;
-
-    public ProductService(ProductRepository productRepository){
-        this.productRepository = productRepository;
-    }
-    public List<Product> getAll(){
-        return null;
-    }
-
-    public Product add(ProductForAddDto productForAddDto){
-        //mapping
-        Product product = new Product();
-        product.setName(productForAddDto.getName());
-        product.setDescription(productForAddDto.getDescription());
-        product.setStock(productForAddDto.getStock());
-        product.setPrice(productForAddDto.getUnitPrice());
-
-        Category category = new Category();
-        category.setId(productForAddDto.getCategoryId());
-
-        product.setCategory(category);
-        return this.productRepository.save(product);
-
-    }
 }
