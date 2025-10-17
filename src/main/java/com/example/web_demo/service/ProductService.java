30ceba84498f30e4dabadcb10d1b1b4c4ac381c3
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-17 23:41:28 +0300

    CreatedProductResponse's add function added

--- src/main/java/com/example/web_demo/service/ProductService.java
+++ src/main/java/com/example/web_demo/service/ProductService.java
@@ -1,8 +1,42 @@
 package com.example.web_demo.service;
 
+import com.example.web_demo.dto.product.request.CreateProductRequest;
+import com.example.web_demo.dto.product.response.CreatedProductResponse;
+import com.example.web_demo.entity.Category;
+import com.example.web_demo.entity.Product;
+import com.example.web_demo.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService{
 
+    // repository bağımlılığı
+    // final: yalnızca construvtor üzerinden set edilebilir olması için
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public CreatedProductResponse add(CreateProductRequest createProductRequest){
+        // add logic
+        Product product = new Product();
+
+        product.setName(createProductRequest.getName());
+        product.setUnitPrice(createProductRequest.getUnitPrice());
+        product.setStock(createProductRequest.getStock());
+        product.setDescription(createProductRequest.getDescription());
+
+        //TODO: Fix
+        Category category = new Category();
+        category.setId(createProductRequest.getCategoryId());
+
+        product.setCategory(category);
+        productRepository.save(product);
+        return new CreatedProductResponse(product.getId(),
+                                            product.getName(),
+                                            product.getStock(),
+                                            product.getDescription(),
+                                            product.getUnitPrice());
+    }
 }
