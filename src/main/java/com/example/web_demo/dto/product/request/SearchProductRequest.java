c297b247c3c9fbbfb2976e6b214eb223db087ac9
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-21 23:01:21 +0300

    SearchProduct DTOs added

---
+++ src/main/java/com/example/web_demo/dto/product/request/SearchProductRequest.java
@@ -0,0 +1,13 @@
+package com.example.web_demo.dto.product.request;
+
+public class SearchProductRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
