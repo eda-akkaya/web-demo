4897603b0098bf9bd13d68eb7935d7d5e4b6dc60
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-21 23:01:59 +0300

    GetByIdProductResponse added

---
+++ src/main/java/com/example/web_demo/dto/product/response/GetByIdProductResponse.java
@@ -0,0 +1,84 @@
+package com.example.web_demo.dto.product.response;
+
+public class GetByIdProductResponse {
+    private int id;
+    private String name;
+    private float stock;
+    private String description;
+    private float unitPrice;
+    private int categoryId;
+    private String categoryName;
+
+    public GetByIdProductResponse() {
+    }
+
+    public GetByIdProductResponse(int id, String name,
+                                  float stock,
+                                  String description,
+                                  float unitPrice,
+                                  int categoryId, String categoryName) {
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getStock() {
+        return stock;
+    }
+
+    public void setStock(float stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
