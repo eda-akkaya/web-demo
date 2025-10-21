fdd965c1bb5fd17c24bc162ecece75c5d31a5332
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-21 22:55:46 +0300

    getById and search method added

--- src/main/java/com/example/web_demo/controller/ProductsController.java
+++ src/main/java/com/example/web_demo/controller/ProductsController.java
@@ -1,10 +1,15 @@
 package com.example.web_demo.controller;
 
 import com.example.web_demo.dto.product.request.CreateProductRequest;
+import com.example.web_demo.dto.product.request.SearchProductRequest;
 import com.example.web_demo.dto.product.response.CreatedProductResponse;
+import com.example.web_demo.dto.product.response.GetByIdProductResponse;
+import com.example.web_demo.dto.product.response.SearchProductResponse;
 import com.example.web_demo.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController //Spring tarafından RestController olarak tanınsın
 @RequestMapping("/api/v1/products") // localhost:port/api/v1/products ile başlıyorsa istek buraya gelir.
@@ -21,4 +26,15 @@ public class ProductsController {
     public CreatedProductResponse add(@RequestBody CreateProductRequest createProductRequest){
         return productService.add(createProductRequest);
     }
+
+    @GetMapping("{id}")
+    public GetByIdProductResponse getById(@PathVariable int id){
+        return productService.getById(id);
+    }
+    @GetMapping("search")
+    public List<SearchProductResponse> search(SearchProductRequest request){
+        return productService.search(request);
+    }
+
+
 }
