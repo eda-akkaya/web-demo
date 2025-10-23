2f29c49c7910b4ede3e6ad051782b2ddc8c71bf5
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-23 16:05:42 +0300

    validation notation added to controller

--- src/main/java/com/example/web_demo/controller/ProductsController.java
+++ src/main/java/com/example/web_demo/controller/ProductsController.java
@@ -6,6 +6,7 @@ import com.example.web_demo.dto.product.response.CreatedProductResponse;
 import com.example.web_demo.dto.product.response.GetByIdProductResponse;
 import com.example.web_demo.dto.product.response.SearchProductResponse;
 import com.example.web_demo.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +24,7 @@ public class ProductsController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CreatedProductResponse add(@RequestBody CreateProductRequest createProductRequest){
+    public CreatedProductResponse add(@Valid @RequestBody CreateProductRequest createProductRequest){
         return productService.add(createProductRequest);
     }
 
